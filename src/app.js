6f65337e548c38cc06e9f46447b675827181fc4a
aakritigkmit <aakriti@gkmit.co>
2024-10-25 12:16:52 +0530

    feat:update app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const { connectToDB } = require('./config/database.js');
+const express = require("express");
+const dotenv = require("dotenv");
 
-
-
+const { connectToDB } = require("./config/database.js");
+const authRoutes = require("./routes/users.route.js");
 dotenv.config();
+
 const app = express();
 app.use(express.json());
 
-connectToDB();
+app.use("/api", authRoutes);
 
+connectToDB();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
